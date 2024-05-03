8c9f1c931769ea81a482d8c4edd1cf5a1fd451c1
dag-andersen <dagbjerreandersen@gmail.com>
2024-05-03 21:46:42 +0200

    Moved the manifest extraction logic into its own file

---
+++ src/extract.rs
@@ -0,0 +1,173 @@
+use std::collections::HashSet;
+use std::fs;
+use std::{collections::BTreeMap, error::Error};
+
+use log::{debug, error, info};
+
+use crate::utils::{run_command, run_command_output_to_file};
+use crate::{apply_manifest, apps_file, Branch};
+
+static ERROR_MESSAGES: [&str; 6] = [
+    "helm template .",
+    "authentication required",
+    "authentication failed",
+    "path does not exist",
+    "error converting YAML to JSON",
+    "Unknown desc = `helm template .",
+];
+
+static TIMEOUT_MESSAGES: [&str; 2] = ["Client.Timeout", "failed to get git client for repo"];
+
+pub async fn get_resources(
+    branch_type: &Branch,
+    timeout: u64,
+    output_folder: &str,
+) -> Result<(), Box<dyn Error>> {
+    info!("🌚 Getting resources for {}", branch_type);
+
+    let app_file = apps_file(branch_type);
+
+    if fs::metadata(app_file).unwrap().len() != 0 {
+        match apply_manifest(app_file) {
+            Ok(_) => (),
+            Err(e) => panic!("error: {}", String::from_utf8_lossy(&e.stderr)),
+        }
+    }
+
+    let mut set_of_processed_apps = HashSet::new();
+    let mut set_of_failed_apps = BTreeMap::new();
+
+    let start_time = std::time::Instant::now();
+
+    loop {
+        let output = run_command("kubectl get applications -n argocd -oyaml", None)
+            .await
+            .expect("failed to get applications");
+        let applications: serde_yaml::Value =
+            serde_yaml::from_str(&String::from_utf8_lossy(&output.stdout)).unwrap();
+
+        let items = applications["items"].as_sequence().unwrap();
+        if items.is_empty() {
+            break;
+        }
+
+        let mut list_of_timed_out_apps = vec![];
+
+        let mut apps_left = 0;
+
+        for item in items {
+            let name = item["metadata"]["name"].as_str().unwrap();
+            if set_of_processed_apps.contains(name) {
+                continue;
+            }
+            match item["status"]["sync"]["status"].as_str() {
+                Some("OutOfSync") | Some("Synced") => {
+                    debug!("Processing application: {}", name);
+                    match run_command_output_to_file(
+                        &format!("argocd app manifests {}", name),
+                        &format!("{}/{}/{}", output_folder, branch_type, name),
+                        false,
+                    )
+                    .await
+                    {
+                        Ok(_) => debug!("Processed application: {}", name),
+                        Err(e) => error!("error: {}", String::from_utf8_lossy(&e.stderr)),
+                    }
+                    set_of_processed_apps.insert(name.to_string().clone());
+                    continue;
+                }
+                Some("Unknown") => {
+                    if let Some(conditions) = item["status"]["conditions"].as_sequence() {
+                        for condition in conditions {
+                            if let Some("ComparisonError") = condition["type"].as_str() {
+                                match condition["message"].as_str() {
+                                    Some(msg) if ERROR_MESSAGES.iter().any(|e| msg.contains(e)) => {
+                                        set_of_failed_apps
+                                            .insert(name.to_string().clone(), msg.to_string());
+                                        continue;
+                                    }
+                                    Some(msg)
+                                        if TIMEOUT_MESSAGES.iter().any(|e| msg.contains(e)) =>
+                                    {
+                                        list_of_timed_out_apps.push(name.to_string().clone());
+                                    }
+                                    _ => (),
+                                }
+                            }
+                        }
+                    }
+                }
+                _ => (),
+            }
+            apps_left += 1
+        }
+
+        if apps_left == 0 {
+            break;
+        }
+
+        let time_elapsed = start_time.elapsed().as_secs();
+        if time_elapsed > timeout as u64 {
+            error!("❌ Timed out after {} seconds", timeout);
+            error!(
+                "❌ Processed {} applications, but {} applications still remain",
+                set_of_processed_apps.len(),
+                apps_left
+            );
+            return Err("Timed out".into());
+        }
+
+        if !set_of_failed_apps.is_empty() {
+            for (name, msg) in &set_of_failed_apps {
+                error!(
+                    "❌ Failed to process application: {} with error: \n{}",
+                    name, msg
+                );
+            }
+            return Err("Failed to process applications".into());
+        }
+
+        if !list_of_timed_out_apps.is_empty() {
+            info!(
+                "💤 {} Applications timed out.",
+                list_of_timed_out_apps.len(),
+            );
+            for app in &list_of_timed_out_apps {
+                match run_command(&format!("argocd app get {} --refresh", app), None).await {
+                    Ok(_) => info!("🔄 Refreshing application: {}", app),
+                    Err(e) => error!(
+                        "⚠️ Failed to refresh application: {} with {}",
+                        app,
+                        String::from_utf8_lossy(&e.stderr)
+                    ),
+                }
+            }
+        }
+
+        info!(
+            "⏳ Waiting for {} out of {} applications to become 'OutOfSync'. Retrying in 5 seconds. Timeout in {} seconds...",
+            apps_left,
+            items.len(),
+            timeout - time_elapsed
+        );
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
+
+    info!(
+        "🌚 Got all resources from {} applications for {}",
+        set_of_processed_apps.len(),
+        branch_type
+    );
+
+    match run_command(
+        "kubectl delete applications.argoproj.io -n argocd --all",
+        None,
+    )
+    .await
+    {
+        Ok(_) => (),
+        Err(e) => error!("error: {}", String::from_utf8_lossy(&e.stderr)),
+    }
+    Ok(())
+}
